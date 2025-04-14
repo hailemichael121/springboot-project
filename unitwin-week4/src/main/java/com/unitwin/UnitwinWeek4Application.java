8ecedc4d21432834f12c07667df685f934c366d5
hailemichael121 <yihunaashe@gmail.com>
2025-04-14 10:18:24 +0300

    Add main application class for Week 4 Spring Boot project

---
+++ unitwin-week4/src/main/java/com/unitwin/UnitwinWeek4Application.java
@@ -0,0 +1,11 @@
+package com.unitwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnitwinWeek4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(UnitwinWeek4Application.class, args);
+    }
+}
